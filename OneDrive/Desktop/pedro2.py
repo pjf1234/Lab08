e30358ee7f968d31036f04417bb343c4c6b7d5ce
pjf1234 <128436028+pjf1234@users.noreply.github.com>
2023-03-22 11:49:56 -0400

    first push

---
+++ OneDrive/Desktop/pedro2.py
@@ -0,0 +1,9 @@
+userName = input("Hello! Please enter your facebook username! ") 
+
+userPassword = input("Hello! Please enter your facebook password! ")
+
+if(userName.lower() == "pedro" and userPassword == "ProgrammingRocks!"):
+    print("Welcome to facebook!")
+else: 
+    print("Wrong credentials, try again!")
+
